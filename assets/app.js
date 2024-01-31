170c8fdab2f32b7446e2e1c0ed0e902f4e784287
hugo <hugo.vanhoutte.pro@gmail.com>
2024-01-31 17:14:39 +0100

    Ajout de Bootstrap, création du MainController.php, et des templates _partials: _navbar et _footer

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,12 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+
+/*
+*   Hugo: This is where I import stylesheets, TODO: Is it where I import scripts as well ?
+*/
 import './styles/app.css';
+import './styles/bootstrap.min.css';
+import './scripts/bootstrap.bundle.min.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
